895d357913247969ca247e338ce34cd9cfc19578
Nora8112003 <thelistgaming99@gmail.com>
2023-11-21 21:02:45 +0700

    shoppingonline2

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.z71nubk.mongodb.net',
+    DB_USER: 'nora811',
+    DB_PASS: 'Tich08112003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '0335526732',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'nora1192003@gmail.com', // gmail service
+    EMAIL_PASS: 'ubfbfyswoumncoyc'
+  };
+  module.exports = MyConstants;
